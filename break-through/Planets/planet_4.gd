de6639e6d360dedc891d70a3235cfa46701d7731
AWizardNamedGandalf <AWizardNamedGandalf@proton.me>
2025-10-20 11:26:52 -0700

    Created rotation and velocity for planets

--- break-through/Planets/planet_4.gd
+++ break-through/Planets/planet_4.gd
@@ -1,10 +1,9 @@
 extends RigidBody2D
-var rotation_number = randi_range(-100, 100)
-var velocity_number1 = randi_range(-10, 10)
-var velocity_number2 = randi_range(-10, 10)
+var rotation_number = randi_range(-400, 400)
+var velocity_number1 = randi_range(-40, 40)
+var velocity_number2 = randi_range(-40, 40)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	apply_torque_impulse(rotation_number)
 	apply_central_impulse(Vector2(velocity_number1,velocity_number2))
-	
--- break-through/Planets/planet_7.gd
+++ break-through/Planets/planet_7.gd
@@ -1,10 +1,9 @@
 extends RigidBody2D
-var rotation_number = randi_range(-100, 100)
-var velocity_number1 = randi_range(-10, 10)
-var velocity_number2 = randi_range(-10, 10)
+var rotation_number = randi_range(-700, 700)
+var velocity_number1 = randi_range(-70, 70)
+var velocity_number2 = randi_range(-70, 70)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	apply_torque_impulse(rotation_number)
 	apply_central_impulse(Vector2(velocity_number1,velocity_number2))
-	
