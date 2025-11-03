78d3bdad2dc7dbc37b243e4dbc282fe1f030eb3e
AWizardNamedGandalf <AWizardNamedGandalf@proton.me>
2025-11-02 21:22:22 -0800

    First enemy tracking
    enemy traverses to and from player and the enemy base

--- break-through/World/Enemies/shooter_enemy.gd
+++ break-through/World/Enemies/shooter_enemy.gd
@@ -1,7 +1,9 @@
 extends RigidBody2D
 
 var track : bool = true
+var distance : float = 0
 @onready var player = get_tree().get_nodes_in_group("player")[0]
+@onready var enemy_base = get_tree().get_nodes_in_group("enemy_base")[0]
 @export var speed = 200
 
 func _ready():
@@ -13,11 +15,26 @@ func _physics_process(delta):
 	if player:
 		if track:
 			var direction = (player.global_position - global_position).normalized()
+			distance = player.global_position.distance_to(global_position)
+			print(distance)
+			if distance < 100:
+				apply_central_force(direction * speed)
+			else:
+				track = false
+		if track == false:
+			var direction = (enemy_base.global_position - global_position).normalized()
 			apply_central_force(direction * speed)
 	else:
 		pass
+			
 
 
 func _on_body_entered(body):
-	print("contact", body.name)
-	pass # Replace with function body.
+	if body.is_in_group("player"):
+		print("player contacted")
+		track = false
+	if body.is_in_group("enemy_base"):
+		print("mothership contacted")
+		track = true
+	else:
+		pass
