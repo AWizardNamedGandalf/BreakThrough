aefecf67d320391f0dfdced6528097deb4be5cc9
AWizardNamedGandalf <AWizardNamedGandalf@proton.me>
2025-11-02 20:56:36 -0800

    Checking collisions
    Turned on contact monitor in inspector and connected on body entered signal print collision

--- break-through/World/Enemies/shooter_enemy.gd
+++ break-through/World/Enemies/shooter_enemy.gd
@@ -1,11 +1,23 @@
 extends RigidBody2D
 
+var track : bool = true
 @onready var player = get_tree().get_nodes_in_group("player")[0]
 @export var speed = 200
-func _physics_process(delta):
+
+func _ready():
+	set_contact_monitor(true)
 	if player:
 		print("player found")
-		var direction = (player.global_position - global_position).normalized()
-		apply_central_force(direction * speed)
+	
+func _physics_process(delta):
+	if player:
+		if track:
+			var direction = (player.global_position - global_position).normalized()
+			apply_central_force(direction * speed)
 	else:
 		pass
+
+
+func _on_body_entered(body):
+	print("contact", body.name)
+	pass # Replace with function body.
